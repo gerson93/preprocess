0297a6901733720db586668bff36f45edbb79a31
gerson93 <gerson.garciacampos@gmail.com>
2020-06-08 13:40:28 -0400

    08-06

--- test/test.py
+++ test/test.py
@@ -1,5 +1,13 @@
-#from search import search_by_words
+""" from preprocess.search import search_by_words
 
-#from search.search import search_by_words
+text = 'Pato patito color de café, si tu no me quieres yo ya se porqué, y no me presumas porque yo ya se que tú eres un pato color de café'
 
-from preprocess.search import search_by_words
+s = search_by_words(text, 'color') """
+
+from preprocess.preprocess_basic import preprocessing
+
+text = 'Pato patito color de café, si tu no me quieres yo ya se porqué, y no me presumas porque yo ya se que tú eres un pato color de café'
+
+prep = preprocessing(text)
+
+print(prep.text_())
